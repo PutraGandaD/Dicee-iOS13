271480eccdbc90c3500008b3efbd0742314f96ac
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-09-23 14:21:51 +0700

    Section 4 : 37

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,17 +4,29 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
+//  Studied by Putra Ganda Dewata
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // diceImageViewOne
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix") //use #imageLiteral(
+        diceImageViewOne.alpha = 0.5 // to make a customization of alpha's picture
+        
+        // diceImageViewTwo
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageViewTwo.alpha = 0.5
     }
 
 
 }
-
+ 
